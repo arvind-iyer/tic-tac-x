b96263aa85928511dd8f0aafb2372a7124a94649
Arvind Iyer <arvindi.me@gmail.com>
2015-12-10 21:45:45 +0800

    The app now logs all the active tiles. Going to start working on firebase support

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,10 +1,27 @@
+var board = document.getElementsByClassName('board');
+var tiles = [];
 
-function populateGameBoard(){
-  var board=  document.getElementsByClassName('board');
-  var tiles = [];
-  for(var i = 0; i < 9; i++){
+//Function to print the tile values
+function updateValues() {
+  var selected = [];
+  tiles.forEach(function(tile, index) {
+    if (tile.firstElementChild.active) {
+      selected.push(index);
+    }
+  });
+  console.log(selected);
+}
+
+//Function to initially create the tiles on the screen
+function populateGameBoard() {
+  for (var i = 0; i < 9; i++) {
     var tile = document.createElement('my-tile');
     tiles.push(tile);
     board[0].appendChild(tile);
+    tile.addEventListener('click', updateValues);
   }
 }
+
+
+
+window.addEventListener('load', populateGameBoard);
